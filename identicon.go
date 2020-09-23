0bf02ed63d4f953e8de63692cd8db6485f0641b5
NolionsChen <nolionschen@jetfuel.com.tw>
2020-09-23 14:33:45 +0800

    [clean] 註解整理

--- identicon.go
+++ identicon.go
@@ -33,6 +33,13 @@ type DrawingPoint struct {
 
 type Apply func(Identicon) Identicon
 
+func Generate(input []byte) Identicon {
+	identicon := hashInput(input)
+	identicon = pipe(identicon, buildGrid, pickColor, filterOddSquares, buildPixelMap)
+
+	return identicon
+}
+
 func hashInput(input []byte) Identicon {
 	checkSum := sha256.Sum256(input)
 	return Identicon{
@@ -40,43 +47,49 @@ func hashInput(input []byte) Identicon {
 	}
 }
 
-func pickColor(identicon Identicon) Identicon {
-	rgb := [3]byte{}                 // first we make a byte array with length 3
-	copy(rgb[:], identicon.hash[:3]) // next we copy the first 3 values from the hash to the rgb array
-	identicon.color = rgb            // we than assign it to the color value
-
+func pipe(identicon Identicon, Applies ...Apply) Identicon {
+	for _, f := range Applies {
+		identicon = f(identicon)
+	}
 	return identicon
 }
 
+// 產生網格
 func buildGrid(identicon Identicon) Identicon {
-
-	var grid []byte // Create empty grid
-	// Loop over the hash from the identicon
-	// Increment with 3 (Chunk the array in 3 parts)
-	// this ensures we wont get array out of bounds error and will retrieve exactly 5 chunks of 3
+	var grid []byte
+	// Loop over the hash from the identicon, and increment with 3 (Chunk the array in 3 parts)
 	for i := 0; i < len(identicon.hash) && i+3 <= len(identicon.hash)-1; i += 3 {
-		chunk := make([]byte, 5)           // Create a placeholder for the chunk
+		chunk := make([]byte, 5)
 		copy(chunk, identicon.hash[i:i+3]) // Copy the items from the old array to the new array
 		chunk[3] = chunk[1]                // mirror the second value in the chunk
 		chunk[4] = chunk[0]                // mirror the first value in the chunk
-		grid = append(grid, chunk...)      // append the chunk to the grid
+		grid = append(grid, chunk...)
 	}
 
 	identicon.grid = grid // set the grid property on the identicon
 	return identicon
 }
 
+// 設置填充顏色
+func pickColor(identicon Identicon) Identicon {
+	rgb := [3]byte{}
+	copy(rgb[:], identicon.hash[:3]) // copy first 3 values to the rgb array from identicon hash
+	identicon.color = rgb            // rgb array assign to the color value of Identicon
+
+	return identicon
+}
+
+// 濾掉奇數網格
 func filterOddSquares(identicon Identicon) Identicon {
-	var grid []GridPoint                  // create a placeholder to hold the values of the loop
-	for i, code := range identicon.grid { // loop over the grid
-		if code%2 == 0 { // check if the value is odd or not
-			// create a new Gridpoint where we save the value and the index in the grid
+	var grid []GridPoint
+	for i, code := range identicon.grid {
+		if code%2 == 0 { // check is even or odd
 			point := GridPoint{
 				value: code,
 				index: i,
 			}
 
-			grid = append(grid, point) // append the item to the new grid
+			grid = append(grid, point)
 		}
 	}
 	// set the property
@@ -84,16 +97,17 @@ func filterOddSquares(identicon Identicon) Identicon {
 	return identicon
 }
 
+// 建立圖示
 func buildPixelMap(identicon Identicon) Identicon {
-	var drawingPoints []DrawingPoint // define placeholder for drawingpoints
+	var drawingPoints []DrawingPoint
 
 	// Closure, this function returns a Drawingpoint
 	pixelFunc := func(p GridPoint) DrawingPoint {
-		horizontal := (p.index % 5) * 50 // This is the formula, we use the index from the gridpoint to calculate the horizontal dimension
-		vertical := (p.index / 5) * 50   // This is the formula, we use the index from the gridpoint to calculate the vertical dimension
+		horizontal := (p.index % 5) * 50 // use the index from the gridpoint to calculate the horizontal dimension
+		vertical := (p.index / 5) * 50   // use the index from the gridpoint to calculate the vertical dimension
 
-		topLeft := Point{horizontal, vertical}               // this is the topleft point with x and the y
-		bottomRight := Point{horizontal + 50, vertical + 50} // the bottom right point is just the topleft point +50 because 1 block in the grid is 50x50
+		topLeft := Point{horizontal, vertical}               // top left
+		bottomRight := Point{horizontal + 50, vertical + 50} // bottom right（top left point +50)
 
 		return DrawingPoint{
 			topLeft,
@@ -110,43 +124,24 @@ func buildPixelMap(identicon Identicon) Identicon {
 }
 
 func rect(img *image.RGBA, col color.Color, x1, y1, x2, y2 float64) {
-	gc := draw2dimg.NewGraphicContext(img) // Prepare new image context
-	gc.SetFillColor(col)                   // set the color
-	gc.MoveTo(x1, y1)                      // move to the topleft in the image
-	// Draw the lines for the dimensions
+	gc := draw2dimg.NewGraphicContext(img)
+	gc.SetFillColor(col) // 設值顏色
+	gc.MoveTo(x1, y1)    // 讓圖片靠左上角
 	gc.LineTo(x1, y1)
 	gc.LineTo(x1, y2)
+
 	gc.MoveTo(x2, y1) // move to the right in the image
-	// Draw the lines for the dimensions
 	gc.LineTo(x2, y1)
 	gc.LineTo(x2, y2)
-	// Set the linewidth to zero
 	gc.SetLineWidth(0)
-	// Fill the stroke so the rectangle will be filled
 	gc.FillStroke()
 }
 
-func pipe(identicon Identicon, funcs ...Apply) Identicon {
-	for _, applyer := range funcs {
-		identicon = applyer(identicon)
-	}
-	return identicon
-}
-
-func Generate(input []byte) Identicon {
-	identicon := hashInput(input)
-	identicon = pipe(identicon, pickColor, buildGrid, filterOddSquares, buildPixelMap)
-
-	return identicon
-}
-
+// 產生Identicon 圖示
 func (i Identicon) WriteImage(w io.Writer) error {
-	// We create our default image containing a 250x250 rectangle
 	var img = image.NewRGBA(image.Rect(0, 0, 250, 250))
-	// We retrieve the color from the color property on the identicon
 	col := color.RGBA{R: i.color[0], G: i.color[1], B: i.color[2], A: 255}
 
-	// Loop over the pixelmap and call the rect function with the img, color and the dimensions
 	for _, pixel := range i.pixelMap {
 		rect(
 			img,
